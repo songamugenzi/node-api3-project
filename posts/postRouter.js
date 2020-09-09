8d1bad354650cfc2a57ff972a8e6689ef4271d8f
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2020-09-09 00:27:35 -0400

    Completed MVP and Stretch goal 1: Implement all endpoints and middleware within posts/postRouter.js

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,84 @@
-const express = require('express');
+const express = require("express");
+
+const PostDb = require("../posts/postDb.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // do your magic!
+// READ & get list of posts //
+router.get("/", (req, res) => {
+  PostDb.get()
+    .then((postList) => {
+      res.status(200).json({ postList });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+// READ & get specific post //
+router.get("/:id", validatePostId, (req, res) => {
+  const validPost = req.post;
+  res.status(200).json({ validPost });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+// DELETE specific post //
+router.delete("/:id", validatePostId, (req, res) => {
+  const deletePost = req.params.id;
+  PostDb.remove(deletePost)
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({ message: "post successfully deleted" });
+      } else {
+        res
+          .status(404)
+          .json({ message: "post with the specified ID does not exist" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "post could not be removed" });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+// UPDATE & put new post changes in the database //
+router.put("/:id", validatePostId, (req, res) => {
+  const changes = req.body;
+  const updatedPost = req.params.id;
+
+  if (!changes.text) {
+    res.status(400).json({ message: "Please provide text" });
+  }
+  PostDb.update(updatedPost, changes)
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({ count, message: "post successfully updated" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "post could not be updated" });
+    });
 });
 
-// custom middleware
+// CUSTOM MIDDLEWARE //
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  const { id } = req.params;
+  PostDb.getById(id)
+    .then((post) => {
+      if (!post) {
+        res.status(400).json({ message: "invalid post id" });
+      } else {
+        req.post = post;
+        next();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: "There was an error retrieving the post from the database",
+      });
+    });
 }
 
 module.exports = router;
